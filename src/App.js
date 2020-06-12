f007eebea6e9a17d8f5ed07d81785664dd0d01b1
sumit <sumit_pandey@outlook.com>
2020-06-12 15:38:40 +0530

    added addtion game

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import React from "react";
-import Greet from "./Greet";
-const App = () => {
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [num1, setNum1] = useState(1);
+  const [num2, setNum2] = useState(1);
+  const [score, setScore] = useState(0);
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState(false);
+
+  const onKeypress = (e) => {
+    const answer = parseInt(response);
+    if (e.key == "Enter") {
+      if (answer === num1 + num2) {
+        setScore(score + 1);
+        setNum1(Math.ceil(Math.random() * 10));
+        setNum2(Math.ceil(Math.random() * 10));
+        setResponse("");
+        setError(false);
+      } else {
+        setError(true);
+      }
+    }
+  };
+
+  if (score >= 5) {
+    return (
+      <div className="container">
+        <h1 style={{ color: "white" }}>Yoohoo! You won!!</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <Greet name="Sam" age="20" gender="male">
-        <p>This is a para</p>
-      </Greet>
-      <Greet name="Luna" age="15" gender="female">
-        <button>Click me</button>
-      </Greet>
-      <Greet name="John" age="18" gender="male" />
-      <Greet name="Amy" age="20" gender="female" />
-      <Greet name="xyz" age="25" gender="male" />
+    <div className="container">
+      <div className="child">
+        <h1>
+          {num1} + {num2}
+        </h1>
+        <input
+          type="text"
+          value={response}
+          placeholder="answer"
+          onChange={(e) => setResponse(e.target.value)}
+          onKeyDown={onKeypress}
+        />
+        {error ? <p style={{ color: "red" }}>opps you are wrong</p> : ""}
+        <h5>Score: {score}</h5>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
